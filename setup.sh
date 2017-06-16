6aaba8b081dd9bc42e1d65390b0525831c1ffeb3
Connor Ruggles <cruggles@iastate.edu>
2017-06-16 14:53:29 -0500

    set up adding ~/bin to PATH on setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,15 @@ bindir="$HOME/bin"
 
 mkdir -p $bindir
 cp install/* $bindir
+
+# export PATH variable
+if [ -f ~/.profile ]; then
+	if [[ ! ":$PATH:" == *":$HOME/bin"* ]]; then
+		printf "export PATH=$PATH:$bindir\n" >> ~/.profile
+	fi
+
+else
+	printf "export PATH=$PATH:$bindir\n" >> ~/.profile
+fi
+
+source ~/.profile
